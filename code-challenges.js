6c17ce41a8d66acfdada509acc7589b4aac6eb2b
kirkkwang <k3wang@gmail.com>
2021-12-03 15:09:55 -0800

    assessment complete

--- code-challenges.js
+++ code-challenges.js
@@ -1,3 +1,7 @@
+/////////////////////////
+// I wanted to challenge myself by wrapping everything in a function, I know we haven't learned about it yet so I've included the way I approached solving the problems without funcitons at the end -Kirk
+/////////////////////////
+
 // ASSESSMENT 1: Coding practical questions
 
 // To run file: navigate to the correct directory in terminal, $ node code-challenges.js
@@ -9,31 +13,133 @@
 
 // Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 42
-// var temp = 350
-// var temp = 212
+const boilingPoint = (temperature) => {
+  if (temperature > 212) {
+    return `${temperature} is above boiling point`;
+  } else if (temperature < 212) {
+    return `${temperature} is below boiling point`;
+  } else {
+    return `${temperature} is at boiling point`;
+  }
+};
 
+// var temp = 42;
+// var temp = 350;
+var temp = 212;
+console.log(boilingPoint(temp));
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 42, 9, 13]
+const lengthOfCombinedArrays = (array1, array2) => {
+  var arraysCombined = array1.concat(array2);
+  return arraysCombined.length;
+};
 
+var myNumbers1 = [3, 7, 0, 36, -9];
+var myNumbers2 = [8, -7, 42, 9, 13];
+console.log(lengthOfCombinedArrays(myNumbers1, myNumbers2));
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
 
-var currentCohort = "Echo 2021"
+const reverseString = (string) => {
+  return currentCohort.split("").reverse().join("");
+};
 
+var currentCohort = "Echo 2021";
+console.log(reverseString(currentCohort));
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-var myArray = [13, 34, 5, 10, 27, 42]
+const oddOrEvenNumbersInArray = (array) => {
+  if (array % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
 
+const loopThroughArray = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(oddOrEvenNumbersInArray(array[i]));
+  }
+};
+
+var myArray = [13, 34, 5, 10, 27, 42];
+loopThroughArray(myArray); // I couldn't figure out how to make it work with wrapping a console.log around the call so I put it inside the loopThroughArray function, I realize this may be a little impractical...
 
 // --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
 
-var number1 = 58
-var number2 = 100
+// var number1 = 58;
+// var number2 = 100;
 
-// var number1 = 27
-// var number2 = 24
+const subtarctSmallerNumFromBiggerNum = (number1, number2) => {
+  if (number1 > number2) {
+    return number1 - number2;
+  } else if (number2 > number1) {
+    return number2 - number1;
+  } else {
+    return "Your numbers are equal!";
+  }
+};
+
+var number1 = 27;
+var number2 = 24;
+console.log(subtarctSmallerNumFromBiggerNum(number1, number2));
+
+/////////////////////////
+// Just in case I messed something up, here's my original answers that don't use functions -Kirk
+/////////////////////////
+
+// --------------------1) Create a statement that determines if a temperature is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit.
+
+// Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+
+// var temp = 42;
+// var temp = 350;
+var temp = 212;
+
+if (temp > 212) {
+  console.log(`${temp} is above boiling point`);
+} else if (temp < 212) {
+  console.log(`${temp} is below boiling point`);
+} else {
+  console.log(`${temp} is at boiling point`);
+}
+
+// --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+
+var myNumbers1 = [3, 7, 0, 36, -9];
+var myNumbers2 = [8, -7, 42, 9, 13];
+var myNumbersCombined = myNumbers1.concat(myNumbers2);
+console.log(myNumbersCombined.length);
+
+// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
+
+var currentCohort = "Echo 2021";
+console.log(currentCohort.split("").reverse().join(""));
+
+// --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
+
+var myArray = [13, 34, 5, 10, 27, 42];
+for (let i = 0; i < myArray.length; i++)
+  if (myArray[i] % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+
+// --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
+
+// var number1 = 58;
+// var number2 = 100;
+
+var number1 = 27;
+var number2 = 24;
+
+if (number1 > number2) {
+  console.log(number1 - number2);
+} else if (number2 > number1) {
+  console.log(number2 - number1);
+} else {
+  console.log("Your numbers are equal!");
+}
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 1: Tech Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory as if you were in a job interview.
@@ -7,60 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+Your answer: Git a tool created by the same creator as Linux used for version control so multiple developers can easily code on the same project. GitHub is a website that people can create projects and repositories on which utilizes the Git technology.
 
-  Researched answer:
-
-
+Researched answer: Git is software created by Linus Torvalds that can track changes in sets of files usually between developers working on the same source code. GitHub is a website, now owned by Microsoft, that hosts version control which uses Git.
 
 2. What types of evaluations will return a Boolean value?
 
-  Your answer:
+Your answer: A Boolean value returned by checking if something is true or false. This can be used in if/else if/else statements to execute code based on specific conditions.
 
-  Researched answer:
-
-
+Researched answer:
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+Your answer: An index is a position while a value is the thing at that position. For example in a string of "kirk", the value of index 0 is "k" since computers start counting at 0 instead of 1.
 
-  Researched answer:
-
-
+Researched answer: An index is the position of an item in a sequence represented numerically such as in an array or a string. A value is a piece of data that can be manipulated.
 
 4. What is iteration?
 
-  Your answer:
+Your answer: Iteration is the repitition of something. It is used in loops and each time the code loops will be a different iteration.
 
-  Researched answer:
-
-
+Researched answer: Iteration is an algorithmic process of steps that occur one after another.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+Your answer: Proper pair programming techniques involve two programmers, a "driver" and a "navigator". The driver will be the person doing the keystrokes and inputing code while the navigator keeps an eye on the overall goal or destination. The navigator helps with catching typos and working through errors by talking through it with the driver. The navigator also do some research while the driver is busy with the code.
 
-  Researched answer:
+Researched answer: Proper pair programming techniques include two programmers working together where one is the "driver" and the other is the "observer"/"navigator". The navigator will review the code as the driver types it in. The two programmers will often switch roles.
 
+6. What is something we did in class this week you found helpful?
 
-
-6. What is something we did in class this week you found helpful?  
-
-  Your answer:
-
-
+Your answer: I really enjoyed watching the Delta cohort present their Capstone projects. It was very motivating and eye opening at how much more we have to go and can go in this course.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. GitHub pull request:
+1. GitHub pull request: A pull request or PR a request ticket where you want to merge your branch into main. Reviewers would take a look at your changes and make sure it will not break anything once merged. It is also where the reviewers can give critiques.
 
-2. Higher Order Functions:
+2. Higher Order Functions: Higher order functions are functions in which the argument that gets passed through is another function and not just a value. A higher order fuction can also return another function.
 
-3. Jest:
+3. Jest: Jest is a testing framework created by Facebook to test JavaScript.
 
-4. Objects:
+4. Objects: Objects are containers for values like a variable. However, unlike a variable, objects can contain many values. The values are paired with keys in order to be accessed. In other languages, a JavaScript object can be called a dictionary.
 
-5. Classes:
+5. Classes: Classes are templates or blueprints for creating objects. Classes must be defined before they can be constructed, unlike functions.
